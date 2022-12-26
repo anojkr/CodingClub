bf70eec39b47b059364cd2fbb7e5b4b95f729057
Anoj Kumar <anoj.kumar48@gmail.com>
2022-12-26 22:31:56 +0530

    reafctor

--- CodingQuestions/Hashing/LongestSubstringWithoutRepeatingChar.py
+++ CodingQuestions/Hashing/LongestSubstringWithoutRepeatingChar.py
@@ -1,24 +1,24 @@
 import collections
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        HashMap = collections.defaultdict(int)
+
+        HashMap = {}
         i = 0
         j = 0
+        maxRes = 0
         N = len(s)
-        res = 0
         while i < N and j < N:
             ele = s[j]
             if ele in HashMap:
-                HashMap[ele] += 1
-                while HashMap[ele] > 1:
-                    if HashMap[s[i]] == 1:
-                        HashMap.pop(s[i])
-                    else:
-                        HashMap[s[i]] -= 1
-                    i += 1
-                j += 1
+                while s[i] != ele:
+                    HashMap.pop(s[i])
+                    i+=1
+                HashMap.pop(s[i])
+                HashMap[ele]=True
+                i+=1
+                j+=1
             else:
-                HashMap[ele] += 1
-                res = max(res, j - i + 1)
-                j += 1
-        return res
+                maxRes = max(maxRes, j-i+1)
+                HashMap[ele]=True
+                j+=1
+        return maxRes
